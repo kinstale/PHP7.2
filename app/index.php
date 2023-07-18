e61e835d612cac448aa89cbaf3d3708d3e719a29
kinstale <112486215+kinstale@users.noreply.github.com>
2023-07-19 00:04:49 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -3,12 +3,12 @@
 body{
     background-image: url('https://i.pinimg.com/originals/ba/ed/f6/baedf62652e907096d24104efc1f7795.gif');
     background-size: cover;
+    background-repeat: no-repeat;
     
     
-    
-    height: 100vh;
-    padding:0;
-    margin:0;
+    height: auto;
+    padding:auto;
+    margin:auto;
 }
 </style>
 <body>
