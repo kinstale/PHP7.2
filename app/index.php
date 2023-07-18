822d3feb8656124032afc56bf96f40f1def97774
kinstale <112486215+kinstale@users.noreply.github.com>
2023-07-19 00:00:31 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,5 +1,19 @@
-<?php
+<html>
+<style>
+body{
+    background-image: url('https://i.pinimg.com/originals/ba/ed/f6/baedf62652e907096d24104efc1f7795.gif');
+    background-size: cover;
+    
+    
+    
+    height: 100vh;
+    padding:0;
+    margin:0;
+}
+</style>
+<body>
 
-echo "This application works!";
 
-?>
+
+</body>
+</html>
